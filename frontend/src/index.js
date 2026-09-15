62ec39c074bab29a48d23e90bdd5bcea3c76345f
sonukumar <sonu9508kr@gmail.com>
2026-09-15 16:01:50 +0530

    Add Zerodha frontend project

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <h1>Hello</h1>
+);
